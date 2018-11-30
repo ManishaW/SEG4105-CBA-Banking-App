c21bd5348ec12fa9f4a0ba5a90d281f7bf7944cb
Lucia <lucia.okeh@gmail.com>
2018-11-30 09:47:21 -0500

    created contact us and made stuff nicer

---
+++ CBAApp/app/src/main/java/com/lucia/cba_app/ContactUs.java
@@ -0,0 +1,22 @@
+package com.lucia.cba_app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.manisha.cba_app.R;
+
+public class ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+
+    }
+
+}
