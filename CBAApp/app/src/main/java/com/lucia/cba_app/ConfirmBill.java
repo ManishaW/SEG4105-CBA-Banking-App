f788513228404c16b76ded7837b1762e5fed0876
Lucia <lucia.okeh@gmail.com>
2018-11-30 09:15:33 -0500

    theme it up

--- CBAApp/app/src/main/java/com/lucia/cba_app/ConfirmBill.java
+++ CBAApp/app/src/main/java/com/lucia/cba_app/ConfirmBill.java
@@ -1,11 +1,13 @@
 package com.lucia.cba_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.manisha.cba_app.R;
 
@@ -16,6 +18,19 @@ public class ConfirmBill extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_bill);
 
+
+        Button back = (Button) findViewById(R.id.button13);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoBack();
+            }
+        });
+    }
+
+    void GoBack(){
+        Intent myIntent = new Intent(this, ViewBillsActivity.class);
+        startActivity(myIntent);
     }
 
 }
